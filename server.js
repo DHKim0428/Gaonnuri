2fe0c098ffa2922b34e56c27ea796dc7effb774d
DHKim <sidsince0428@gmail.com>
2017-08-21 17:48:21 +0900

    server.js 생성

---
+++ server.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+var http = require('http');
+var express = require('express');
+//var app = express();
+//app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    //res.redirect('TntWkDiRn.html');
+    res.send("<h1>HELLO HEROKU</h1>");
+})
+
+
+http.createServer(app).listen(process.env.PORT || 8080, function () {
+   console.log('Server running at http://sugarcanesoft.iptime.org');
+});
